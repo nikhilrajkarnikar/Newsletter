54c15fbab9eb699fa6f129eb311ad9934d08f6fd
nikhilrajkarnikar <52517903+nikhilrajkarnikar@users.noreply.github.com>
2019-11-22 20:42:48 +0545

    firstdraft

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url: "https://us4.api.mailchimp.com/3.0/lists/0cfc42cd45",
+        method: "POST",
+        headers: {
+            "Authorization" : "nik1 90313206f67a0cb920f6d84c74217895-us4"
+        },
+        body: jsonData
+    }
+
+    request(options, function(error, response, body){
+        if (error){
+            console.log(error);
+        } else {
+            console.log(response.statusCode);
+        }
+    });
+
+    // console.log(firstName, lastName, email);
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
+
+// API KEY
+// 90313206f67a0cb920f6d84c74217895-us4
+
+// List ID
+// 0cfc42cd45
